a33e3e233b86364abe04309b5bba7028df0fa500
Trevor Host <hostx009@umn.edu>
2019-05-23 11:07:19 -0400

    Update polyMerge.v1.py

--- workers/polyMerge.v1.py
+++ workers/polyMerge.v1.py
@@ -10,7 +10,10 @@ arcpy.env.overwriteOutput = True
 
 start_time = time.time()
 
-# pass input and output file names 
+# pass input and output file names
+# first arg, input directory
+# second arg, height strata integer (2, 5, 10, 15, 20) 
+# third arg, output file name (SHP) 
 inFile = sys.argv[1]
 strata = sys.argv[2]
 outFile = sys.argv[3]
@@ -19,14 +22,8 @@ outFile = sys.argv[3]
 #outFile = "C:\\Users\\hostx009\\Documents\\temp\\temp.shp"
 
 # Local variables:
-temp = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb"
 temp1 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp1"
 temp2 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp2"
-temp3 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp3"
-temp4 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp4"
-temp5 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp5"
-temp6 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp6"
-temp7 = "C:\\Users\\hostx009\\Documents\\ArcGIS\\Default1.gdb\\polyMerge_temp7"
 
 arcpy.env.workspace = inFile
 List = arcpy.ListFeatureClasses("*clean.shp")
@@ -49,11 +46,6 @@ arcpy.PolygonToRaster_conversion(temp2, "z", outFile, "CELL_CENTER", "NONE", "1"
 # # Process: Delete
 arcpy.Delete_management(temp1, "")
 arcpy.Delete_management(temp2, "")
-# arcpy.Delete_management(temp3, "")
-# arcpy.Delete_management(temp4, "")
-# arcpy.Delete_management(temp5, "")
-# arcpy.Delete_management(temp6, "")
-# arcpy.Delete_management(temp7, "")
 
 # report time
 elapsed_min = (time.time() - start_time)/60
